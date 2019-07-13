64e7aba3397181e8dcb25287dbaec9bac8b2abd1
Salim Attari <testyouriqmind@gmail.com>
2019-07-13 21:50:39 +0500

    Android studio login form 3

---
+++ app/src/main/java/e/salimsp/loginform3/MainActivity.java
@@ -0,0 +1,15 @@
+package e.salimsp.loginform3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
